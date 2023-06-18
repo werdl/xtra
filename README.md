aa072481b0f1565f16cc3cab25ce91dc958114fa
werdl <werdl_@outlook.com>
2023-06-18 19:11:47 +0100

    gtgogn

---
+++ README.md
@@ -0,0 +1,3 @@
+## xtra
+### My personal C macro favourites
+- A little C library that has a load of QOL definitions
--- src/defs.h
+++ src/defs.h
@@ -1,6 +1,13 @@
 #define and &&
 #define or ||
 #define not !
+#define nor !(a or b)
+#define xor ^
+#define bnot ~
+#define band &
+#define bor |
+#define lshi <<
+#define rshi >>
 
 #define is ==
 #define isnt !=
@@ -11,3 +18,11 @@
     #define then {
     #define fi } // for the Bash programmer
 #endif
+
+#define mainf int main(char ** argv,char * argc)
+#define ret0 return 0
+#define ret1 return 1
+
+#define reterr return -1
+#define ifthis(func,...) if(func(...))
+#define ifnthis if(!func(...))
