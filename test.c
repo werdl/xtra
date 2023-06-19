53ad9a1d864df0662ff87b49b2dca0e7639f2e52
werdl <116349156+werdl@users.noreply.github.com>
2023-06-19 15:37:56 +0000

    update example

--- test.c
+++ test.c
@@ -2,10 +2,10 @@
 
 int main() {
     XtraVector * MyVec=NewXtraVector(16);
-    XtraVectorPush(&MyVec,3);
-    XtraVectorPush(&MyVec,4);
-    XtraVectorPush(&MyVec,5);
-    XtraVectorPush(&MyVec,6);
+    XtraVectorPush(&MyVec,(void *)3);
+    XtraVectorPush(&MyVec,(void *)4);
+    XtraVectorPush(&MyVec,(void *)5);
+    XtraVectorPush(&MyVec,(void *)6);
     printf("%p",XtraVectorIndex(&MyVec,2));
     MyVec=XtraVectorDelEl(&MyVec,2);
     printf("%p",XtraVectorIndex(&MyVec,2));
